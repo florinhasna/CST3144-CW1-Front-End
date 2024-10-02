8e047b8ce5133f66e0252b7c719ad482aa40c35d
Florin Hasna <florinhasna@outlook.com>
2024-10-02 15:11:28 +0100

    Added product interaction and checkout page toggle

--- script/script.js
+++ script/script.js
@@ -1,12 +1,30 @@
 var myApp = new Vue({
     el: "#body",
     data: {
-        cartIconAlt: "Cart",
+        showProducts: true, // property for toggling between elements
+        cartIconAlt: "Cart", 
         cartIconSrc: "assets/cart.png",
-        objectsInCart: 0,
-        title: "Painting class",
-        imgSrc: "assets/paint.png",
-        location: "London",
-        price: 100
-    }
+        objectsInCart: [],
+        product: {
+            id: 1234,
+            title: "Painting",
+            imgSrc: "assets/paint.png",
+            location: "London",
+            price: 100,
+            availability: 5, 
+        }
+    },
+    computed:{ // reactive properties for auto-update
+        isAvailable(){
+            return this.product.availability > this.objectsInCart.length;
+        },
+    },
+    methods:{
+        addItem(){ // add item to the cart
+            this.objectsInCart.push(this.product.id);
+        },
+        showCheckout(){ // toggle between products and checkout
+            this.showProducts = this.showProducts ? false : true;
+        }
+    },
 });
