99c6d96032ccf83468b92dd2e3b13e124c3890eb
Florin Hasna <florinhasna@outlook.com>
2024-10-08 18:39:13 +0100

    Added product diversity, updated checkout form.

--- script/script.js
+++ script/script.js
@@ -5,37 +5,108 @@ var myApp = new Vue({
         cartIconAlt: "Cart", 
         cartIconSrc: "assets/cart.png",
         objectsInCart: [],
-        product: {
-            id: 1234,
+        productList: [{
+            id: 1000,
             title: "Painting",
             imgSrc: "assets/paint.png",
             location: "London",
             price: 100,
             availability: 5, 
-        },
+        },{
+            id: 1001,
+            title: "Math",
+            imgSrc: "assets/math.png",
+            location: "London",
+            price: 75,
+            availability: 5, 
+        },{
+            id: 1002,
+            title: "English - Grammar",
+            imgSrc: "assets/english.png",
+            location: "London",
+            price: 65,
+            availability: 5, 
+        },{
+            id: 1003,
+            title: "Programming",
+            imgSrc: "assets/programming.png",
+            location: "London",
+            price: 200,
+            availability: 5, 
+        },{
+            id: 1004,
+            title: "MFL - Modern Foreign Languages",
+            imgSrc: "assets/mfl.png",
+            location: "London",
+            price: 150,
+            availability: 5, 
+        },{
+            id: 1005,
+            title: "Modern Arts",
+            imgSrc: "assets/arts.png",
+            location: "London",
+            price: 75,
+            availability: 5, 
+        },{
+            id: 1006,
+            title: "PE - Physical Education",
+            imgSrc: "assets/pe.png",
+            location: "London",
+            price: 45,
+            availability: 5, 
+        },{
+            id: 1007,
+            title: "Cooking",
+            imgSrc: "assets/cooking.png",
+            location: "London",
+            price: 35,
+            availability: 5, 
+        },{
+            id: 1008,
+            title: "Biology",
+            imgSrc: "assets/biology.png",
+            location: "London",
+            price: 55,
+            availability: 5, 
+        },{
+            id: 1009,
+            title: "Psychology",
+            imgSrc: "assets/psychology.png",
+            location: "London",
+            price: 70,
+            availability: 5, 
+        },{
+            id: 1010,
+            title: "Music",
+            imgSrc: "assets/music.png",
+            location: "London",
+            price: 110,
+            availability: 5, 
+        },],
         order: {
-            firstName: '',
-            lastName: '',
-            address: '',
-            city: '',
-            county: '',
-            postcode: '',
-            method: '',
+            name: '',
             phoneNo: '',
-            email: '',
         }
     },
     computed:{ // reactive properties for auto-update
-        isAvailable(){
-            return this.product.availability > this.objectsInCart.length;
-        },
+        
     },
     methods:{
-        addItem(){ // add item to the cart
-            this.objectsInCart.push(this.product.id);
+        addItem(id){ // add item to the cart
+            this.objectsInCart.push(id);
         },
         showCheckout(){ // toggle between products and checkout
             this.showProducts = this.showProducts ? false : true;
+        },
+        cartCount(productID) {
+            count = 0;
+
+            this.objectsInCart.forEach(element => {
+                if(element === productID)
+                    count++;
+            });
+
+            return count;
         }
     },
 });
