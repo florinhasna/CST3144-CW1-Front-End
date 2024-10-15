3e146d8e2f4d169cd7fc749a0c385975a24c7452
Florin Hasna <florinhasna@outlook.com>
2024-10-15 20:36:43 +0100

    Changed all images with Font Awesome svg icons.

--- script/script.js
+++ script/script.js
@@ -3,93 +3,106 @@ var myApp = new Vue({
     data: {
         showProducts: true, // property for toggling between elements
         cartIconAlt: "Cart", 
-        cartIconSrc: "assets/cart.png",
+        cartIconSrc: "assets/cart.svg",
         objectsInCart: [],
         productList: [{
             id: 1000,
             title: "Painting",
-            imgSrc: "assets/paint.png",
-            location: "London",
+            imgSrc: "assets/paint.svg",
+            location: "Uxbridge",
             price: 100,
             availability: 5, 
-        },{
+        },
+        {
             id: 1001,
             title: "Math",
-            imgSrc: "assets/math.png",
-            location: "London",
+            imgSrc: "assets/math.svg",
+            location: "Harrow",
             price: 75,
             availability: 5, 
-        },{
+        },
+        {
             id: 1002,
             title: "English - Grammar",
-            imgSrc: "assets/english.png",
-            location: "London",
+            imgSrc: "assets/english.svg",
+            location: "Watford",
             price: 65,
             availability: 5, 
-        },{
+        },
+        {
             id: 1003,
             title: "Programming",
-            imgSrc: "assets/programming.png",
-            location: "London",
+            imgSrc: "assets/programming.svg",
+            location: "Hendon",
             price: 200,
             availability: 5, 
-        },{
+        },
+        {
             id: 1004,
             title: "MFL - Modern Foreign Languages",
-            imgSrc: "assets/mfl.png",
-            location: "London",
+            imgSrc: "assets/mfl.svg",
+            location: "Uxbridge",
             price: 150,
             availability: 5, 
-        },{
+        },
+        {
             id: 1005,
             title: "Modern Arts",
-            imgSrc: "assets/arts.png",
-            location: "London",
+            imgSrc: "assets/arts.svg",
+            location: "Hendon",
             price: 75,
             availability: 5, 
-        },{
+        },
+        {
             id: 1006,
             title: "PE - Physical Education",
-            imgSrc: "assets/pe.png",
-            location: "London",
+            imgSrc: "assets/pe.svg",
+            location: "Watford",
             price: 45,
             availability: 5, 
-        },{
+        },
+        {
             id: 1007,
             title: "Cooking",
-            imgSrc: "assets/cooking.png",
-            location: "London",
+            imgSrc: "assets/cooking.svg",
+            location: "Harrow",
             price: 35,
             availability: 5, 
-        },{
+        },
+        {
             id: 1008,
             title: "Biology",
-            imgSrc: "assets/biology.png",
-            location: "London",
+            imgSrc: "assets/biology.svg",
+            location: "Harrow",
             price: 55,
             availability: 5, 
-        },{
+        },
+        {
             id: 1009,
             title: "Psychology",
-            imgSrc: "assets/psychology.png",
-            location: "London",
+            imgSrc: "assets/psychology.svg",
+            location: "Watford",
             price: 70,
             availability: 5, 
-        },{
+        },
+        {
             id: 1010,
             title: "Music",
-            imgSrc: "assets/music.png",
-            location: "London",
+            imgSrc: "assets/music.svg",
+            location: "Uxbridge",
             price: 110,
             availability: 5, 
-        },],
+        }],
         order: {
             name: '',
             phoneNo: '',
-        }
+        },
+        search: '',
     },
     computed:{ // reactive properties for auto-update
-        
+        isCartEmpty() {
+            return (this.objectsInCart.length === 0);
+        }
     },
     methods:{
         addItem(id){ // add item to the cart
